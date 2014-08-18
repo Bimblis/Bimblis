1d69ea8777f308277d7fbe1332bdf77bda103f8d
Zakaria Boutami <zboutami@redsauce.net>
2014-08-18 10:58:23 +0200

    Javascript interaction
    Added support for javascript interaction: click an element, choose an
    option, fill a text field and check a checkbox.

--- lib/features/step_definitions/web_shared_steps.rb
+++ lib/features/step_definitions/web_shared_steps.rb
@@ -11,12 +11,20 @@ Then (/In (.*?), I click the element of (.*?)$/) do |page, element|
   on_page(page + 'Page').click_element(element)
 end
 
+Then (/In (.*?), I click internally the element of (.*?)$/) do |page, element|
+  on_page(page + 'Page').click_element(element, true)
+end
+
 Then (/In (.*?), I hover the mouse over element (.*?)$/) do |page, element|
   on_page(page + 'Page').hover_element(element)
 end
 
 Then (/In (.*?), I choose option (.*?) in the select (.*?)$/) do |page, option, element|
   on_page(page + 'Page').select_element(option, element)
+end
+
+Then (/In (.*?), I choose internally option (.*?) in the select (.*?)$/) do |page, option, element|
+  on_page(page + 'Page').select_element(option, element, true)
 end
 
 Then (/In (.*?), I check the (.*?) of element (.*?)$/) do |page, existance, element|
@@ -27,8 +35,16 @@ Given(/^In (.*?), I fill (.*?) in text field (.*?)$/) do |page, lorem, element|
   on_page(page + 'Page').input_text_field(lorem, element)
 end
 
+Given(/^In (.*?), I fill internally (.*?) in text field (.*?)$/) do |page, lorem, element|
+  on_page(page + 'Page').input_text_field(element, lorem, true)
+end
+
 Then (/In (.*?), I (.*?) the checkbox of (.*?)$/) do |page, check, element|
   on_page(page + 'Page').check_checkbox(check, element)
+end
+
+Then (/In (.*?), I (.*?) internally the checkbox of (.*?)$/) do |page, check, element|
+  on_page(page + 'Page').check_checkbox(check, element, true)
 end
 
 Then (/I should be in webpage (.*?)$/) do |url|
